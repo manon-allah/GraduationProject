2bc6c95fd5642f13d7814ce87b152ff029e18bcb
menna allah <menatallah260@gmail.com>
2024-11-02 19:00:21 +0200

    search ui

--- lib/features/search/presentation/screens/search_screen.dart
+++ lib/features/search/presentation/screens/search_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../chatting/presentation/screens/widgets/custom_search_bar.dart';
+
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
@@ -10,9 +12,17 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Search Screen'),
-      )
-    ;
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 55,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: CustomSearchBar(),
+        ),
+      ],
+    );
   }
 }
