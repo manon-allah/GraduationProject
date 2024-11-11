da0c06d6898380f671f51a227510e6469b6efc43
menna allah <menatallah260@gmail.com>
2024-11-11 21:46:30 +0200

    user model

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,6 +1,19 @@
-
 class UserModel {
-  String? uid;
-  String? password;
-  String? email;
+  final String uid;
+  final String password;
+  final String email;
+
+  UserModel({
+    required this.uid,
+    required this.password,
+    required this.email,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'password': password,
+      'email': email,
+    };
+  }
 }
