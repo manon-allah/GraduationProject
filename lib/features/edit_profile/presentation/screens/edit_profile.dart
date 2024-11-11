ad2b66dc78e41b8812a59ba236c9b4ec38e5f10e
menna allah <menatallah260@gmail.com>
2024-11-11 22:56:48 +0200

    custom bar in edit screen

--- lib/features/edit_profile/presentation/screens/edit_profile.dart
+++ lib/features/edit_profile/presentation/screens/edit_profile.dart
@@ -6,42 +6,8 @@ class EditProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.white,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        // backgroundColor: const Color(0xffFAFAFA),
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Cancel',
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Color(0xFF606060),
-                ),
-              ),
-            ),
-            const Text('Edit Profile'),
-            InkWell(
-              onTap: () {},
-              child: const Text(
-                'Done',
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: const CustomEditProfile(),
+    return const Scaffold(
+      body: CustomEditProfile(),
     );
   }
 }
